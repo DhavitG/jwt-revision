83119036f1309407196e943c76f38d5ff20ce2e0
DhavitG <gdhavit@gmail.com>
2025-03-26 19:12:11 +0530

    final error handling

--- controllers/main.js
+++ controllers/main.js
@@ -1,11 +1,11 @@
 const jwt = require("jsonwebtoken");
-const CustomAPIError = require("../errors/custom-error");
+const BadRequest = require("../errors/bad-request");
 
-const login = (req, res) => {
+const login = async (req, res) => {
   const { username, password } = req.body;
 
   if (!username || !password) {
-    throw new CustomAPIError("Please provide email and password", 400);
+    throw new BadRequest("Please provide email and password");
   }
 
   const id = new Date().getDate();
@@ -18,11 +18,11 @@ const login = (req, res) => {
 
 const dashboard = async (req, res) => {
   const lucky = Math.floor(Math.random() * 100);
-  res
-    .status(200)
-    .json({
-      msg: `Hello ${req.user.username}. Here is your authorized data, your lucky number is ${lucky}`,
-    });
+  console.log("User Data:", req.user);
+  res.status(200).json({
+    msg: `Hello, ${req.user.username}`,
+    secret: `Here is your authorized data, your lucky number is ${lucky}`,
+  });
 };
 
 module.exports = {
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,4 +1,5 @@
 const CustomAPIError = require("../errors/custom-error");
+const { StatusCodes } = require("http-status-codes");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomAPIError) {
@@ -6,7 +7,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   }
 
   return res
-    .status(500)
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
     .send(`Something has gone wrong, and that is ${err.message}`);
 };
 
